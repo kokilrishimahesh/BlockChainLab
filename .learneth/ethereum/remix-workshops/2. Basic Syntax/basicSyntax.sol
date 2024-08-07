27e135f34aad8a52cb835ed47e074db47c1a816d
kokilrishimahesh <2021.rishi.kokil@ves.ac.in>
2024-08-07 23:32:20 +0530

    Completed Second Module basicSyntax.sol .  Also completed the assignment.

--- .learneth/ethereum/remix-workshops/2. Basic Syntax/basicSyntax.sol
+++ .learneth/ethereum/remix-workshops/2. Basic Syntax/basicSyntax.sol
@@ -2,6 +2,10 @@
 // compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
 pragma solidity ^0.8.3;
 
-contract HelloWorld {
-    string public greet = "Hello World!";
+contract MyContract{
+    // Assignment
+    // 1. Rename your contract to MyContract
+    // 2. Creating a public string variable name
+    // 3. Assigning it a value as Alice
+    string public name = "Alice";
 }
