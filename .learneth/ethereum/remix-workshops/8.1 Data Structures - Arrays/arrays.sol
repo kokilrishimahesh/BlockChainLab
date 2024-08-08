466d6494076f8324120af998608cb4585bbe0747
kokilrishimahesh <2021.rishi.kokil@ves.ac.in>
2024-08-08 23:44:14 +0530

    Completed Module 8.1 arrays.sol and also completed the assignment

---
+++ .learneth/ethereum/remix-workshops/8.1 Data Structures - Arrays/arrays.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract Array {
+    // Several ways to initialize an array
+    uint256[] public arr;
+    uint256[] public arr2 = [1, 2, 3];
+    // Fixed sized array, all elements initialize to 0
+    uint256[10] public myFixedSizeArr;
+
+    function get(uint256 i) public view returns (uint256) {
+        return arr[i];
+    }
+
+    // Solidity can return the entire array.
+    // But this function should be avoided for
+    // arrays that can grow indefinitely in length.
+
+
+    // Assignment
+    uint[3] public arr3 = [0, 1, 2];
+
+    function  getArr() public view returns(uint[3] memory){
+        return arr3;
+    }
+
+    function push(uint256 i) public {
+        // Append to array
+        // This will increase the array length by 1.
+        arr.push(i);
+    }
+
+    function pop() public {
+        // Remove last element from array
+        // This will decrease the array length by 1
+        arr.pop();
+    }
+
+    function getLength() public view returns (uint256) {
+        return arr.length;
+    }
+
+    function remove(uint256 index) public {
+        // Delete does not change the array length.
+        // It resets the value at index to it's default value,
+        // in this case 0
+        delete arr[index];
+    }
+}
+
+contract CompactArray {
+    uint256[] public arr;
+
+    // Deleting an element creates a gap in the array.
+    // One trick to keep the array compact is to
+    // move the last element into the place to delete.
+    function remove(uint256 index) public {
+        // Move the last element into the place to delete
+        arr[index] = arr[arr.length - 1];
+        // Remove the last element
+        arr.pop();
+    }
+
+    function test() public {
+        arr.push(1);
+        arr.push(2);
+        arr.push(3);
+        arr.push(4);
+        // [1, 2, 3, 4]
+
+        remove(1);
+        // [1, 4, 3]
+
+        remove(2);
+        // [1, 4]
+    }
+
+}
