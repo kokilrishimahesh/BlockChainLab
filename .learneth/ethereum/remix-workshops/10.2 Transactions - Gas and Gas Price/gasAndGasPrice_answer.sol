5aade8082f803aa36ac5388919cea56728cf449e
kokilrishimahesh <2021.rishi.kokil@ves.ac.in>
2024-08-09 00:55:22 +0530

    Completed module 10.2 gasAndPrice.sol  Also completed the assignment

--- .learneth/ethereum/remix-workshops/10.2 Transactions - Gas and Gas Price/gasAndGasPrice_answer.sol
+++ .learneth/ethereum/remix-workshops/10.2 Transactions - Gas and Gas Price/gasAndGasPrice_answer.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.3;
 
 contract Gas {
     uint public i = 0;
+    uint public cost = 170367;
 
     // Using up all of the gas that you send causes your transaction to fail.
     // State changes are undone.
@@ -14,9 +15,4 @@ contract Gas {
             i += 1;
         }
     }
-
-    // Assignment
-    uint public cost = 170367; //The value of cost = 170367 in the Gas contract 
-    // represents the amount of gas required to deploy the contract on the Ethereum 
-    // blockchain. 
 }
